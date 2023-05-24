d29a2cd5780730f0a1a2bd16dacd3af8fb8c4e9c
pronab pal <palpronab@gmail.com>
2023-05-25 01:48:01 +1000

    Update processnext.sh
    Signed-off-by: pronab pal <palpronab@gmail.com>

--- SimpleFlow_Sequencer/processnext.sh
+++ SimpleFlow_Sequencer/processnext.sh
@@ -1,3 +1,3 @@
-<--!not used ..generated dynamicallu in logic in subExec.js -->
+<--!not used ..generated dynamicallu in logic in subExec.js https://linuxhint.com/define-models-on-nodejs-server/ https://mongoosejs.com/docs/index.html -->
 echo "Starting unitrigger!"  
 node e_UnitEndTrigger.js > unitEndLog.txt 
